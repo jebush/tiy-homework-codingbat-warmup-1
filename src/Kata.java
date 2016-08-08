6c5a4a2fe2e21695d4f661d73449815a24b9aec5
Jimmy Bush <jebush94@hotmail.com>
2016-08-08 17:35:37 -0400

    this is done

--- src/Kata.java
+++ src/Kata.java
@@ -110,8 +110,231 @@ public class Kata {
     }
 
     public String missingChar(String str, int n) {
+        String front = str.substring(0, n);
+
+        String back = str.substring(n+1, str.length());
+
+        return front + back;
+    }
+
+    public String frontBack(String str) {
+        if (str.length() <= 1) return str;
+
+        String mid = str.substring(1, str.length()-1);
+
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
 
     }
+
+    public String front3(String str) {
+        if (str.length() <= 3){
+            return str + str + str;
+        }
+        else {
+            String first = str.substring(0,3);
+
+            return first + first + first;
+        }
+
+    }
+
+    public String backAround(String str) {
+        String last = str.substring(str.length() - 1);
+        return last + str + last;
+    }
+
+    public boolean or35(int n) {
+        return (n % 3 == 0) || (n % 5 == 0);
+    }
+
+    public String front22(String str) {
+        int thing = 2;
+        if (thing > str.length()) {
+            thing = str.length();
+        }
+
+        String front = str.substring(0, thing);
+        return front + str + front;
+
+    }
+
+    public boolean startHi(String str) {
+
+        if (str.startsWith("hi")) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean icyHot(int temp1, int temp2) {
+        if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean in1020(int a, int b) {
+        return ((a >= 10 && a <= 20) || (b >= 10 && b <= 20));
+    }
+
+    public boolean hasTeen(int a, int b, int c) {
+        return (a>=13 && a<=19) ||
+                (b>=13 && b<=19) ||
+                (c>=13 && c<=19);
+    }
+
+    public boolean loneTeen(int a, int b) {
+        boolean aRig = (a >= 13 && a <= 19);
+        boolean bRig = (b >= 13 && b <= 19);
+
+        return (aRig && !bRig) || (!aRig && bRig);
+
+    }
+
+    public String delDel(String str) {
+        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
+
+            return str.substring(0, 1) + str.substring(4);
+        }
+        else {
+            return str;
+        }
+    }
+
+    public boolean mixStart(String str) {
+
+        if (str.length() < 3) return false;
+
+        String mixing = str.substring(1,3);
+
+        if (mixing.equals("ix")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public String startOz(String str) {
+        String theEnd = "";
+
+        if (str.length() >= 1 && str.charAt(0)=='o') {
+            theEnd = theEnd + str.charAt(0);
+        }
+
+        if (str.length() >= 2 && str.charAt(1)=='z') {
+            theEnd = theEnd + str.charAt(1);
+        }
+
+        return theEnd;
+    }
+
+    public int intMax(int a, int b, int c) {
+        int maxNum;
+
+
+        if (a > b) {
+            maxNum = a;
+        } else {
+            maxNum = b;
+        }
+
+        if (c > maxNum) {
+            maxNum = c;
+        }
+
+        return maxNum;
+    }
+
+    public int close10(int a, int b) {
+
+        int aDiff = Math.abs(a - 10);
+        int bDiff = Math.abs(b - 10);
+
+        if (aDiff < bDiff){
+            return a;
+        }
+        else if (bDiff < aDiff){
+            return b;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public boolean in3050(int a, int b) {
+        if (a >= 30 && a <= 40 && b >= 30 && b <= 40) {
+            return true;
+        }
+        if (a >= 40 && a <= 50 && b >= 40 && b <= 50) {
+            return true;
+        }
+        return false;
+    }
+    public int max1020(int a, int b) {
+
+        if (b > a) {
+            int dude = a;
+            a = b;
+            b = dude;
+        }
+
+        if (a >= 10 && a <= 20){
+            return a;
+        }
+        if (b >= 10 && b <= 20){
+            return b;
+        }
+        return 0;
+
+    }
+
+    public boolean stringE(String str) {
+
+        int count = 0;
+
+        for (int i=0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e') count++;
+
+        }
+
+        return (count >= 1 && count <= 3);
+    }
+
+    public boolean lastDigit(int a, int b) {
+
+        return (a % 10 == b % 10);
+    }
+    public String endUp(String str) {
+        if (str.length() <= 3) return str.toUpperCase();
+        int cut = str.length() - 3;
+        String front = str.substring(0, cut);
+        String back  = str.substring(cut);  // this takes from cut to the end
+
+        return front + back.toUpperCase();
+    }
+
+    public String everyNth(String str, int n) {
+        String result = "";
+
+        // Look at every nth char
+        for (int i=0; i<str.length(); i = i + n) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -101,20 +101,183 @@ public class Main {
         System.out.println(newDay.notString("not bad"));
 
         System.out.println();
+        System.out.println("Missing Char");
+
+        //This calls the missingChar method
+        System.out.println(newDay.missingChar("kitten", 1));
+        System.out.println(newDay.missingChar("kitten", 0));
+        System.out.println(newDay.missingChar("kitten", 4));
+
         System.out.println();
+        System.out.println("FrontBack");
+
+        //This calls the front back method
+        System.out.println(newDay.frontBack("These"));
+        System.out.println(newDay.frontBack("Dude"));
+        System.out.println(newDay.frontBack("Well"));
+
+        System.out.println();
+        System.out.println("Front 3");
+
+        //This calls the front3 method
+        System.out.println(newDay.front3("This"));
+        System.out.println(newDay.front3("Timme"));
+        System.out.println(newDay.front3("abc"));
+
+        System.out.println();
+        System.out.println("Back Around");
 
         //This calls the
-        System.out.println(newDay);
-        System.out.println(newDay);
-        System.out.println(newDay);
+        System.out.println(newDay.backAround("cat"));
+        System.out.println(newDay.backAround("Dude"));
+        System.out.println(newDay.backAround("Dying"));
 
         System.out.println();
-        System.out.println();
+        System.out.println("Or 3 5");
 
         //This calls the
-        System.out.println(newDay);
-        System.out.println(newDay);
-        System.out.println(newDay);
+        System.out.println(newDay.or35(3));
+        System.out.println(newDay.or35(5));
+        System.out.println(newDay.or35(23));
+
+        System.out.println();
+        System.out.println("front 22");
+
+        //This calls the
+        System.out.println(newDay.front22("Thisbe"));
+        System.out.println(newDay.front22("thisis"));
+        System.out.println(newDay.front22("timeing"));
+
+        System.out.println();
+        System.out.println("Start Hi");
+
+        //This calls the startHi
+        System.out.println(newDay.startHi("hi my name is"));
+        System.out.println(newDay.startHi("dudd"));
+        System.out.println(newDay.startHi("Duded hi"));
+
+        System.out.println();
+        System.out.println("Icy Hot");
+
+        //This calls the icyHot
+        System.out.println(newDay.icyHot(-1, 109));
+        System.out.println(newDay.icyHot(-1, 200));
+        System.out.println(newDay.icyHot(0, 99));
+
+        System.out.println();
+        System.out.println("int 1020");
+
+        //This calls the in1020
+        System.out.println(newDay.in1020(2, 15));
+        System.out.println(newDay.in1020(44, 10));
+        System.out.println(newDay.in1020(23, 23));
+
+        System.out.println();
+        System.out.println("has teen");
+
+        //This calls the hasTeen
+        System.out.println(newDay.hasTeen(12, 13, 14));
+        System.out.println(newDay.hasTeen(22, 23, 24));
+        System.out.println(newDay.hasTeen(13, 13 ,13));
+
+        System.out.println();
+        System.out.println("Lone Teen");
+
+        //This calls the loneTeen
+        System.out.println(newDay.loneTeen(13, 13));
+        System.out.println(newDay.loneTeen(14, 77));
+        System.out.println(newDay.loneTeen(88, 22));
+
+        System.out.println();
+        System.out.println("Delete del");
+
+        //This calls the delDel
+        System.out.println(newDay.delDel("adelbjk"));
+        System.out.println(newDay.delDel("kdelasd"));
+        System.out.println(newDay.delDel("dfdel"));
+
+        System.out.println();
+        System.out.println("Mix Start");
+
+        //This calls the
+        System.out.println(newDay.mixStart("mixitup"));
+        System.out.println(newDay.mixStart("pixitup"));
+        System.out.println(newDay.mixStart("m1xitup"));
+
+
+        System.out.println();
+        System.out.println("Start OZ");
+
+        //This calls the startOZ
+        System.out.println(newDay.startOz("ozman"));
+        System.out.println(newDay.startOz("jzd"));
+        System.out.println(newDay.startOz("omsk"));
+
+
+        System.out.println();
+        System.out.println("intMax");
+
+        //This calls the
+        System.out.println(newDay.intMax(1, 2, 4));
+        System.out.println(newDay.intMax(34, 55, 32));
+        System.out.println(newDay.intMax(2, 44, 76));
+
+
+        System.out.println();
+        System.out.println("Close 10");
+
+        //This calls the close10
+        System.out.println(newDay.close10(22, 44));
+        System.out.println(newDay.close10(10,10));
+        System.out.println(newDay.close10(2, 11));
+
+        System.out.println();
+        System.out.println("in3050");
+
+        //This calls the in3050 method
+        System.out.println(newDay.in3050(30, 40));
+        System.out.println(newDay.in3050(30, 45));
+        System.out.println(newDay.in3050(41, 49));
+
+        System.out.println();
+        System.out.println("max 1020");
+
+        //This calls the
+        System.out.println(newDay.max1020(12,14));
+        System.out.println(newDay.max1020(2, 20));
+        System.out.println(newDay.max1020(21,5));
+
+        System.out.println();
+        System.out.println("e String");
+
+        //This calls the
+        System.out.println(newDay.stringE("theee"));
+        System.out.println(newDay.stringE("Crepe"));
+        System.out.println(newDay.stringE("eeeeee"));
+
+        System.out.println();
+        System.out.println("Last Digit");
+
+        //This calls the lastDigit method
+        System.out.println(newDay.lastDigit(15, 5));
+        System.out.println(newDay.lastDigit(22, 5));
+        System.out.println(newDay.lastDigit(37, 29));
+
+        System.out.println();
+        System.out.println("end Up");
+
+        //This calls the
+        System.out.println(newDay.endUp("Thisisl"));
+        System.out.println(newDay.endUp("dude"));
+        System.out.println(newDay.endUp("TIME"));
+
+        System.out.println();
+        System.out.println("every Nth thing");
+
+        //This calls the 
+        System.out.println(newDay.everyNth("thispank", 2));
+        System.out.println(newDay.everyNth("afasdggdsddd", 4));
+        System.out.println(newDay.everyNth("akdkdkdjfiwiwiejfkkdsjfkjasgfg", 3));
 
 
 
